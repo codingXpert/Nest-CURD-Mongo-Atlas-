314571a0b569e6e15caf1fca2bbf9d4098145c2f
Vivek <vivekraj78965.vr@gmail.com>
2023-02-15 21:16:47 +0530

    Atlas DB Connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProdModule } from './prod/prod.module';
 
 @Module({
-  imports: [ProdModule],
+  imports: [
+    ProdModule,
+    MongooseModule.forRoot('mongodb+srv://Vivek:72NuC07qybp0LpDv@cluster0.adf3vri.mongodb.net/nest_db?retryWrites=true&w=majority')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
